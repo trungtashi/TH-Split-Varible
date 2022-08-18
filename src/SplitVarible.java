a44ee54aca57b89a134004894eede19a37acd7ab
trungtashi99 <trungtashi@gmai.com>
2022-08-18 11:09:05 +0700

    Thực hành tách biến

---
+++ src/SplitVarible.java
@@ -0,0 +1,17 @@
+public class SplitVarible {
+        public static String SplitVarible(int number){
+            boolean isFizz = number % 3 ==0;
+            boolean isBuzz = number % 5 ==0;
+            if(isFizz && isBuzz)
+                return "FizzBuzz";
+
+            if(isFizz)
+                return "Fizz";
+
+            if(isBuzz)
+                return "Buzz";
+
+            return number + "";
+        }
+
+}
